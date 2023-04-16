3e99e69029b1c3a3b0bd6ad36f382b522209e8df
OlegSnytko <oleg.snytko.2005@gmail.com>
2023-04-17 01:06:03 +0300

    LAB3

---
+++ LAB3SEMBMP/header.h
@@ -0,0 +1,43 @@
+
+#ifndef LAB3SEMBMP_HEADER_H
+#define LAB3SEMBMP_HEADER_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+typedef struct {
+    unsigned char type[2];
+    unsigned int size;
+    unsigned short reserved1;
+    unsigned short reserved2;
+    unsigned int offset;
+} BMPHeader;
+
+typedef struct {
+    unsigned int size;
+    int width;
+    int height;
+    unsigned short planes;
+    unsigned short bitsPerPixel;
+    unsigned int compression;
+    unsigned int imageSize;
+    int xPixelsPerMeter;
+    int yPixelsPerMeter;
+    unsigned int colorsUsed;
+    unsigned int importantColors;
+} BMPInfoHeader;
+
+void IsBmp(BMPHeader header);
+void Is24Pixel(BMPInfoHeader infoHeader);
+void BmpToNegative( unsigned char* imageData, int imageSize);
+void WriteInBMP(BMPHeader header,  BMPInfoHeader infoHeader, FILE* BMP, unsigned char* imageData, int imageSize);
+void BmpToGrayBlack(unsigned char* imageData, int imageSize);
+void menu(unsigned char* imageData,int imageSize, FILE* BMP, BMPInfoHeader infoHeader, BMPHeader header);
+void gammaCorrection(unsigned char* imageData, int imageSize, double gamma);
+void gammaParam(double* gamma, char* string, double low, double high);
+void validationParam(char* string, int* param, int low, int high);
+void medianFilter(unsigned char *imageData, BMPInfoHeader *infoHeader);
+
+#endif //LAB3SEMBMP_HEADER_H
---
+++ LAB3SEMBMP/main.c
@@ -0,0 +1,46 @@
+
+#include "header.h"
+int main() {
+
+    char fileName[100];
+    printf("enter the name of file: ");
+    scanf("%s", fileName);
+    FILE* BMP = fopen(fileName, "rb");
+
+    if(!(BMP)){
+        printf("Error opening file.\n");
+        return 0;
+    }
+
+    BMPHeader header;
+    BMPInfoHeader infoHeader;
+    fread(&header.type, sizeof(header.type), 1, BMP);
+    fread(&header.size, sizeof(header.size), 1, BMP);
+    fread(&header.reserved1, sizeof(header.reserved1), 1, BMP);
+    fread(&header.reserved2, sizeof(header.reserved2), 1, BMP);
+    fread(&header.offset, sizeof(header.offset), 1, BMP);
+    fread(&infoHeader, sizeof(BMPInfoHeader), 1, BMP);
+
+    IsBmp(header);
+    Is24Pixel(infoHeader);
+
+    int rowSize = ((infoHeader.width * infoHeader.bitsPerPixel + 31) / 32) * 4;
+    int imageSize = rowSize * infoHeader.height;
+
+    unsigned char* imageData = (unsigned char*) malloc(imageSize * sizeof(unsigned char));
+    fread(imageData, sizeof(unsigned char), imageSize, BMP);
+
+    fclose(BMP);
+
+    menu(imageData, imageSize, BMP, infoHeader, header);
+
+    fclose(BMP);
+    free(imageData);
+
+    return 0;
+}
+//George_Floyd.bmp
+
+
+
+
