36506025db4fb4fdae3c6c8c543b2126e110a70a
OlegSnytko <114155853+OlegSnytko@users.noreply.github.com>
2023-04-18 23:19:04 +0300

    Update header.h

--- src/LAB3SEMBMP/header.h
+++ src/LAB3SEMBMP/header.h
@@ -35,7 +35,7 @@ void Is24Pixel(BMPInfoHeader infoHeader);
 void BmpToNegative( unsigned char* imageData, int imageSize);
 void WriteInBMP(BMPHeader header,  BMPInfoHeader infoHeader, FILE* BMP, const unsigned char* imageData, int imageSize);
 void BmpToGrayBlack(unsigned char* imageData, int imageSize);
-void menu(unsigned char* imageData,int imageSize, FILE* BMP, BMPInfoHeader infoHeader, BMPHeader header);
+void menu(const unsigned char* imageData,int imageSize, FILE* BMP, BMPInfoHeader infoHeader, BMPHeader header);
 void gammaCorrection(unsigned char* imageData, int imageSize, double gamma);
 void gammaParam(double* gamma, char* string, double low, double high);
 void validationParam(char* string, int* param, int low, int high);
